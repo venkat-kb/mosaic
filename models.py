5edc544cfe629b0ee70930018ba7dd5d17f641e6
Aryan Tomar <aryantomar03@gmail.com>
2025-06-09 16:35:40 +0530

    Base code

---
+++ models.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from typing import List, Dict
+from dataclasses import dataclass, field
+
+
+class Grievance:
+    caller_name: str
+    caller_phone_no: str
+    description: str
+    location: str
+    date_time: datetime
+
+
+class CaseRecord:
+    case_no: str
+    case_category: str
+    case_detail: str
+    problem_start: str  # or datetime
+    location: str
+    urgency: int
+    scoring: Dict  # specify types if needed e.g., Dict[str, int]
+    category: str
+    thread: List[Grievance] = field(default_factory=list)
+
+
+class Category:
+    name: str
+    priority: int
+
+
+case = CaseRecord(
+    case_no="asdfgh",
+    case_category="",
+    case_detail="Power outage",
+    problem_start="date of last night",
+    location="Lucknow, UP",
+    urgency=0,
+    scoring={},
+    category="Fire",
+    thread=[
+        Grievance(
+            caller_name="Diff name",
+            caller_phone_no=9876543210,
+            description="My building has no power",
+            location="Lucknow",
+            date_time="2025-06-09 12:00",
+        )
+    ],
+)
